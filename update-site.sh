29c77c454142bc3aa35be62011a281112365d034
dominatos <sviatoslavfedorenko@gmail.com>
2026-04-22 17:48:45 +0200

    conf example update

--- update-site.sh
+++ update-site.sh
@@ -1,5 +1,7 @@
 cp /var/www/html/conf.yml /var/www/html/conf.yml-bak --backup=t
 cp /home/sviatoslav/scripts/yaml-dashboard-bot/index.php /var/www/html/index.php --backup=t
 cd /home/sviatoslav/scripts/yaml-dashboard-bot/tg-admin-bot/ && docker compose down && docker compose build --no-cache && docker compose up -d
+cd /home/sviatoslav/scripts/dashy && docker compose down && docker compose up -d
+
 
 
